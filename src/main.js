3326746e938e502db22ef3eb05c8ae09b88d9565
Егор <Egor@MacBook-Pro-Aroslav.local>
2025-06-18 16:57:26 +0300

    add json files with languages

--- src/main.js
+++ src/main.js
@@ -1,26 +1,18 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
-import App from "./App.vue";
 import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+
+import en from "./languages/en.json";
+import ru from "./languages/ru.json";
 
 const i18n = createI18n({
   locale: "ru",
   fallbackLocale: "en",
   messages: {
-    ru: {
-      "title-one":
-        "Поддерживать локализованные проекты так же легко, как и статические.",
-      "title-two": "Расширение IDE управления I18n",
-      "title-three": "Получить для вашей IDE",
-      "language": "Change language to English",
-    },
-    en: {
-      "title-one": "Maintain localized projects as easy as static ones.",
-      "title-two": "I18n management IDE extension",
-      "title-three": "Get for your IDE",
-      "language": "Сменить язык на Русский",
-    },
+    en,
+    ru
   },
 });
 
