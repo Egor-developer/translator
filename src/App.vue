f534121b34508c9e3c1484701cbb2d7366dffbeb
Dmitry Puzyrev <dipuzyrev@gmail.com>
2025-06-18 21:32:12 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,10 @@ export default {
   <div class="relative w-[100dvw] h-[100dvh] flex flex-col justify-center items-center text-center">
     <button class="button absolute p-[20px_50px] rounded-[10px] top-[50px] right-[50px] border-0 text-[var(--color-background)] bg-[var(--color-accent-soft)] cursor-pointer" @click="changeLanguage">{{ t('language') }}</button>
     <h1 class="text-[32px] font-black text-[var(--color-accent)]">
-      {{ t('title-one') }}
+      {{ t('titles.one') }}
     </h1>
-    <h2 class="text-[26px] font-medium text-[var(--color-heading)]">{{ t('title-two') }}</h2>
-    <h3 class="text-[22px] font-light text-[var(--color-text)]">{{ t('title-three') }}</h3>
+    <h2 class="text-[26px] font-medium text-[var(--color-heading)]">{{ t('titles.two') }}</h2>
+    <h3 class="text-[22px] font-light text-[var(--color-text)]">{{ t('titles.three') }}</h3>
   </div>
 </template>
 
